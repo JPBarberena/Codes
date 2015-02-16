2945af6b73a50b68fbc5519a31ac7f0ed0c9adaa
JPBarberena <a01229292@itesm.mx>
2015-02-16 09:34:05 -0600

    Update wsq08.cpp

--- wsq08.cpp
+++ wsq08.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void Plus (int a, int b) {
-    cout << a+b << endl;
+int Plus (int a, int b) {
+  int Plus=a+b;
+    return Plus;
 }
 
-void Substract (int a, int b) {
-    cout << a-b << endl;
+int Substract (int a, int b) {
+  int Substract=a+b;
+    return a-b;
 }
 
-void Product (int a, int b) {
-    cout << a*b << endl;
+int Product (int a, int b) {
+  int Product=a+b;
+    return a*b;
 }
 
-void Division (int a, int b) {
-    cout << a/b << endl;
+int Division (int a, int b) {
+  int Division=a+b;
+    return a/b;
 }
 
-void Remainder (int a, int b) {
-    cout << a%b << endl;
+int Remainder (int a, int b) {
+  int Remainder=a+b;
+    return a%b;
 }
 
 
@@ -30,12 +35,18 @@ int main ()
   cin >> a;
   cout << "Please enter another number" << endl;
   cin >> b;
+  int Sum = Plus (a,b);
+  int Minus = Substract (a,b);
+  int Prod = Product (a,b);
+  int Div = Division (a,b);
+  int Rem = Remainder (a,b);
   cout << "The sum, difference, product, division and remainder of these numbers are: " << endl;
-  Plus (a,b);
-  Substract (a,b);
-  Product (a,b);
-  Division (a,b);
-  Remainder (a,b);
+  cout << Sum << endl;
+  cout << Minus << endl;
+  cout << Prod << endl;
+  cout << Div << endl;
+  cout << Rem << endl;
   return 0;
 
 }
+
